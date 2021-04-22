22b44b8de5c67569b40620c239e0f36077f59a91
유진이 <wlsdk400@naver.com>
2021-04-22 15:42:03 +0900

    S04P31B107 [fix] : FE_0422 esLint 경고 해결

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,9 +1,6 @@
 
 module.exports = {
-    lintOnSave: false
-}
-
-module.exports = {
+    lintOnSave: false,
     devServer: {
         // 사용자 정의 환경 변수에서 VUE_APP_PORT가 있으면 사용하고
         // 없으면 8000 포트로 개발서버를 실행합니다.
