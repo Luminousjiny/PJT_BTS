47704b15bae0c5e98ba49b21426140fe35a6f1bf
유진이 <wlsdk400@naver.com>
2021-04-22 11:05:57 +0900

    S04P31B107-222_[feat] : FE_0422 공통파일 생성, http파일 생성, css파일 생성

--- frontend/src/views/Login/Login.vue
+++ frontend/src/views/Login/Login.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="login">
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
+  name: "Login",
   components: {
-    HelloWorld,
   },
 };
 </script>
