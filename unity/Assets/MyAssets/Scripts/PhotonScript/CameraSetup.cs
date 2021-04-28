69c3222c3bfa876856491907c6739149655b5b03
황호연 <55988698+HoYeonHwang@users.noreply.github.com>
2021-04-28 23:33:11 +0900

    S04P31B107-80 [fix] : UN_0428 - 멀티플레이 마우스 휠 FOV 기능 추가
    Former-commit-id: 2f6f345da65cfec76d20ced0758882fa88448c0b

--- unity/Assets/MyAssets/Scripts/PhotonScript/CameraSetup.cs
+++ unity/Assets/MyAssets/Scripts/PhotonScript/CameraSetup.cs
@@ -6,25 +6,59 @@ using Photon.Pun;
 
 public class CameraSetup : MonoBehaviourPun
 {
+    public float speed = 10.0f;
+    public Transform cameraTrarget;
+
+    private CinemachineVirtualCamera followCam;
+    private Vector3 worldDefalutForward;
+    private const int zoomSpeed = 50;
+
     // Start is called before the first frame update
     void Start()
     {
-            //내가 로컬 플레이어일까? 
+        //내가 로컬 플레이어일까? 
         if (photonView.IsMine)
         {
             // 씬에 있는 시네머신 가상 카메라를 찾고
-            CinemachineVirtualCamera followCam =
+            followCam =
                 FindObjectOfType<CinemachineVirtualCamera>();
             // 가상 카메라의 추적 대상을 자신의 트랜스폼으로 변경
             followCam.Follow = transform;
             followCam.LookAt = transform;
-
-        }      
+            worldDefalutForward = transform.forward;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        // 로컬 플레이어
+        if (photonView.IsMine)
+        {
+            // 스크롤 속도 
+            float scroll = Input.GetAxis("Mouse ScrollWheel") * speed * -1;
+
+            //최대 줌인
+
+            if (followCam.m_Lens.FieldOfView <= 10.0f && scroll < 0)
+            {
+                followCam.m_Lens.FieldOfView = 10.0f;
+            }
+            // 최대 줌 아웃
+
+            else if (followCam.m_Lens.FieldOfView >= 60.0f && scroll > 0)
+            {
+                followCam.m_Lens.FieldOfView = 60.0f;
+            }
+            // 줌인 아웃 하기.
+
+            else
+            {
+                followCam.m_Lens.FieldOfView += scroll;
+            }
+
+
+        }
     }
+
 }
