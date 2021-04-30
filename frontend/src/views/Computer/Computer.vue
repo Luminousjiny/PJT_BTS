3afb19636c7c0d82f157096538ab7b131da19063
hyeji6138 <hyeji6138@naver.com>
2021-05-01 02:50:36 +0900

    S04P31B107-371 [feat] : FE_0501 : 접속한 유저 확인 창 구현
    Former-commit-id: bc01d3392cb9550d7d7cd0e7fe76eff818473c44

--- frontend/src/views/Computer/Computer.vue
+++ frontend/src/views/Computer/Computer.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="computer">
     <Nav />
-    <div id="join" v-if="!session">
+    <div id="join" v-if="!data.session">
       <div id="join-form">
         <label>사용자 이름 : </label>
-        <input type="text" v-model="userName" required />
+        <input type="text" v-model="data.userName" required />
         <label>  방 이름 : </label>
-        <input type="text" v-model="roomName" required />
+        <input type="text" v-model="data.roomName" required />
         <button @click="joinSession">입장</button>
       </div>
     </div>
-    <div id="session" v-if="session">
-        <Chat :user-name="userName" :session="session" :receive-message="receiveMessage" :participants="participants" v-on:sendMessage="send"/>
-        <WebCam :share="share" :room-name="roomName" :OV="OV" :session="session" :publisher="publisher" :subscribers="subscribers" :setting="setting" :user-name="userName" 
-                  v-on:leaveSession="leaveSession"/>
+    <div id="session" v-if="data.session">
+        <Chat :data="data" v-on:sendMessage="send"/>
+        <WebCam :data="data" v-on:leaveSession="leaveSession"/>
     </div>
   </div>
 </template>
@@ -37,91 +36,93 @@ export default {
     },
     data() {
       return {
-        OV: undefined,
-        session: undefined,
-        mainStreamManager: undefined,
-        publisher: undefined,
-        subscribers: [],
-        participants : 1,
-        roomName: "room1",
-        userName: "user1",
-        setting: {
-          audioSource: undefined,
-          videoSource: undefined,
-          publishAudio: false,
-          publishVideo: false,
-          resolution: "640x480",
-          frameRate: 30,
-          insertMode: "APPEND",
-          mirror: false,
-        },
-        receiveMessage: [],
-        share : {
-          active : false,
-          screen : undefined,
+        data : {
+          OV: undefined,
+          session: undefined,
+          mainStreamManager: undefined,
+          publisher: undefined,
+          subscribers: [],
+          participants : 1,
+          roomName: "room1",
+          userName: "user1",
+          setting: {
+            audioSource: undefined,
+            videoSource: undefined,
+            publishAudio: false,
+            publishVideo: false,
+            resolution: "640x480",
+            frameRate: 30,
+            insertMode: "APPEND",
+            mirror: false,
+          },
+          receiveMessage: [],
+          share : {
+            active : false,
+            screen : undefined,
+          },
         },
       }
     },
     destroyed(){
-      this.session = undefined;
-      this.mainStreamManager = undefined;
-      this.publisher = undefined;
-      this.subscribers = [];
-      this.OV = undefined;
-      this.receiveMessage = [];
+      this.data.session = undefined;
+      this.data.mainStreamManager = undefined;
+      this.data.publisher = undefined;
+      this.data.subscribers = [];
+      this.data.OV = undefined;
+      this.data.receiveMessage = [];
     },
     methods: {
       joinSession() {
-        this.OV = new OpenVidu();
+        this.data.OV = new OpenVidu();
         // this.OV.setAdvancedConfiguration({
         //   screenShareChromeExtension: "https://chrome.google.com/webstore/detail/YOUR_EXTENSION_NAME/YOUR_EXTENSION_ID",
         // });
 
-        this.session = this.OV.initSession();
+        this.data.session = this.data.OV.initSession();
 
-        this.session.on("streamCreated", ({ stream }) => {
-          const subscriber = this.session.subscribe(stream);
+        this.data.session.on("streamCreated", ({ stream }) => {
+          const subscriber = this.data.session.subscribe(stream);
           if(subscriber.stream.typeOfVideo == "SCREEN"){
-            this.share.active = true;
-            this.share.screen = subscriber;
+            this.data.share.active = true;
+            this.data.share.screen = subscriber;
           }
-          this.subscribers.push(subscriber);
-          this.participants = this.subscribers.length+1;
+          this.data.subscribers.push(subscriber);
+          this.data.participants = this.data.subscribers.length+1;
         });
-        this.session.on("streamDestroyed", ({ stream }) => {
-          const index = this.subscribers.indexOf(stream.streamManager, 0);
+        this.data.session.on("streamDestroyed", ({ stream }) => {
+          const index = this.data.subscribers.indexOf(stream.streamManager, 0);
           if(stream.typeOfVideo == "SCREEN"){
-            this.share.active = false;
-            this.share.screen = undefined;
+            this.data.share.active = false;
+            this.data.share.screen = undefined;
           }
           if (index >= 0) {
-            this.subscribers.splice(index, 1);
+            this.data.subscribers.splice(index, 1);
           }
-          this.participants = this.subscribers.length+1;
+          this.data.participants = this.data.subscribers.length+1;
         });
-        this.session.on("signal:my-chat", (event) => {
-          this.receiveMessage.push({sender : JSON.parse(event.from.data), message : event.data});
+        this.data.session.on("signal:my-chat", (event) => {
+          this.data.receiveMessage.push({sender : JSON.parse(event.from.data), message : event.data});
           console.log(event.from);
         });
 
-        this.session.on("publisherStartSpeaking", (event) => {
+        this.data.session.on("publisherStartSpeaking", (event) => {
           console.log( "Publisher " + event.connection.connectionId + " start speaking" );
         });
 
-        this.session.on("publisherStopSpeaking", (event) => {
+        this.data.session.on("publisherStopSpeaking", (event) => {
           console.log( "Publisher " + event.connection.connectionId + " stop speaking" );
         });
 
-        this.getToken(this.roomName).then((token) => {
-          this.session
-            .connect(token, { userName: this.userName })
+        this.getToken(this.data.roomName).then((token) => {
+          this.data.session
+            .connect(token, { userName: this.data.userName })
             .then(() => {
-              let publisher = this.OV.initPublisher(undefined, this.setting);
+              let publisher = this.data.OV.initPublisher(undefined, this.data.setting);
 
-              this.mainStreamManager = publisher;
-              this.publisher = publisher;
+              this.data.mainStreamManager = publisher;
+              this.data.publisher = publisher;
 
-              this.session.publish(this.publisher);
+              this.data.session.publish(this.data.publisher);
             })
             .catch((error) => {
               console.log(
@@ -132,18 +133,18 @@ export default {
             });
         });
 
-        window.addEventListener("beforeunload", this.leaveSession);
+        window.addEventListener("beforeunload", this.data.leaveSession);
       },
       leaveSession() {
-        if (this.session) this.session.disconnect();
+        if (this.data.session) this.data.session.disconnect();
 
-        this.session = undefined;
-        this.mainStreamManager = undefined;
-        this.publisher = undefined;
-        this.subscribers = [];
-        this.OV = undefined;
-        this.receiveMessage = [];
-        window.removeEventListener("beforeunload", this.leaveSession);
+        this.data.session = undefined;
+        this.data.mainStreamManager = undefined;
+        this.data.publisher = undefined;
+        this.data.subscribers = [];
+        this.data.OV = undefined;
+        this.data.receiveMessage = [];
+        window.removeEventListener("beforeunload", this.data.leaveSession);
       },
       getToken(roomName) {
         return this.createSession(roomName).then((sessionId) =>
@@ -205,7 +206,7 @@ export default {
         });
       },
       send(sendMessage) {
-        this.session.signal({
+        this.data.session.signal({
             data: sendMessage,
             to: [],
             type: "my-chat",
