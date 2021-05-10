bb16b7f745e58d7e885af1c84c2de02312740c2d
유진이 <wlsdk400@naver.com>
2021-05-10 23:44:49 +0900

    빌드제발
    Former-commit-id: 74a2b2b9e255982ed760ea7464841f8b472bbd51

--- backend/src/test/java/com/ssafy/bts/BtsApplicationTests.java
+++ backend/src/test/java/com/ssafy/bts/BtsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class BtsApplicationTests {
+class BtsApplicationTests {
 
     @Test
     void contextLoads() {
