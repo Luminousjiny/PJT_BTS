d0b6ee2d94d010f1a37f3a0ac1a5e60071e99ba0
hyeji6138 <hyeji6138@naver.com>
2021-05-12 14:33:15 +0900

    유니티 프론트 테스트
    Former-commit-id: 5012eb8c1105fd14a21f22d8a2260c76d93d6108

---
+++ frontend/src/views/Unity/Unity.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <unity
+        src = "/unity/Build/webGL.json"
+        width ="1024"
+        height = "768"
+        unityLoader = "/unity/Build/UnityLoader.js"
+        ref = "hookInstance"
+        >
+
+        </unity>
+    </div>
+</template>
+<script>
+import Unity from 'vue-unity-webgl'
+
+export default {
+    components: { Unity }
+}
+</script>
