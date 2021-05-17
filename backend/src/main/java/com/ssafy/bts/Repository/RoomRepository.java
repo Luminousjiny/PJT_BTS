4658deddbf6a5b8cd069231a1960fd762fb74695
유진이 <wlsdk400@naver.com>
2021-05-18 04:54:56 +0900

    S04P31B107-222 [fix] : 백 수정 완료
    Former-commit-id: a2b70477b8fea1c2771ecec43cd9d2759d773b57

--- backend/src/main/java/com/ssafy/bts/Repository/RoomRepository.java
+++ backend/src/main/java/com/ssafy/bts/Repository/RoomRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
     Room findByRoomId(int roomId);
-    Room findByRoomNumber(int roomNumber);
+    Room findByRoomName(String roomName);
 }
